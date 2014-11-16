481af1793ea75dbf9d7ace79932b355a2d772651
virtuahost <virtualghosts@gmail.com>
2014-11-16 17:10:25 -0500

    Minimap Merged work

---
+++ Assets/Scripts/MinimapUpdater.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapUpdater : MonoBehaviour {
+	public Transform playerPosition;
+	public GameObject MinimapCamera;
+	public GameObject MinimapMarker;
+	private Vector3 lstPosition;
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 newPosition = new Vector3 (playerPosition.position.x, playerPosition.position.y, playerPosition.position.z);
+		if (lstPosition != newPosition) {
+						//transform.position = new Vector3 (playerPosition.position.x, playerPosition.position.y, playerPosition.position.z);
+						 //MinimapCamera.transform.position = newPosition;
+						//Vector3 markerVector3 = new Vector3(MinimapCamera.transform.position.x,MinimapCamera.transform.position.y + 10, MinimapCamera.transform.position.z);
+						//Instantiate (MinimapMarker, markerVector3, MinimapCamera.transform.rotation);
+						lstPosition = newPosition;
+				}
+	}
+}
