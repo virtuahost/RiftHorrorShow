1634d62c21a25689430b3af571d2a1b272584a5c
virtuahost <virtualghosts@gmail.com>
2014-11-16 13:28:50 -0500

    Initial Try for Map

---
+++ Assets/Scripts/MapToggle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapToggle : MonoBehaviour {
+	public GameObject CameraRight;
+	public GameObject CameraLeft;
+	public GameObject OrgCameraRight;
+	public GameObject OrgCameraLeft;
+	public GameObject DirectionalLight;
+	private bool mapMode = false;
+	// Update is called once per frame
+	void Start(){		
+		CameraRight.camera.enabled = mapMode;
+		CameraLeft.camera.enabled = mapMode;
+		DirectionalLight.light.enabled = mapMode;
+		
+		OrgCameraRight.camera.enabled = !mapMode;
+		OrgCameraLeft.camera.enabled = !mapMode;
+		}
+
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.M)){
+			mapMode = !mapMode;
+			
+			CameraRight.camera.enabled = mapMode;
+			CameraLeft.camera.enabled = mapMode;
+			DirectionalLight.light.enabled = mapMode;
+			
+			OrgCameraRight.camera.enabled = !mapMode;
+			OrgCameraLeft.camera.enabled = !mapMode;
+		}	
+	}
+}
---
+++ Assets/Scripts/MinimapUpdater.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapUpdater : MonoBehaviour {
+	public Transform playerPosition;
+	public GameObject rightCamera;
+	// Update is called once per frame
+	void LateUpdate () {
+		//transform.position = new Vector3 (playerPosition.position.x, playerPosition.position.y, playerPosition.position.z);
+		//rightCamera.transform.position = new Vector3 (playerPosition.position.x, playerPosition.position.y, playerPosition.position.z);
+	}
+}
