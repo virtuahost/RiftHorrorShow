1ddc3b7ff0e84941f2bae1a65ff1c6a3ce6e3655
virtuahost <virtualghosts@gmail.com>
2014-11-16 18:39:44 -0500

    Arrow changes check in

--- Assets/Scripts/MinimapUpdater.cs
+++ Assets/Scripts/MinimapUpdater.cs
@@ -9,12 +9,15 @@ public class MinimapUpdater : MonoBehaviour {
 	// Update is called once per frame
 	void LateUpdate () {
 		Vector3 newPosition = new Vector3 (playerPosition.position.x, playerPosition.position.y, playerPosition.position.z);
-		if (lstPosition != newPosition) {
+		//if (lstPosition != newPosition) {
 						//transform.position = new Vector3 (playerPosition.position.x, playerPosition.position.y, playerPosition.position.z);
 						 //MinimapCamera.transform.position = newPosition;
 						//Vector3 markerVector3 = new Vector3(MinimapCamera.transform.position.x,MinimapCamera.transform.position.y + 10, MinimapCamera.transform.position.z);
 						//Instantiate (MinimapMarker, markerVector3, MinimapCamera.transform.rotation);
+						Vector3 markerVector3 = new Vector3(newPosition.x,newPosition.y + 50, newPosition.z);
+						MinimapMarker.transform.position = markerVector3;
+						MinimapMarker.transform.rotation = playerPosition.rotation;
 						lstPosition = newPosition;
-				}
+		//		}
 	}
 }
