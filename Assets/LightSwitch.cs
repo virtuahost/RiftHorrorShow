6de823a46e2d882814e7310b10bf727ba06287f3
Brent <bblihovde3@gatech.edu>
2014-11-16 18:07:50 -0500

    minmap
    minimap fixes

---
+++ Assets/LightSwitch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitch : MonoBehaviour {
+
+	public Light mMLight;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnPreCull ()
+	{
+		mMLight.enabled = true;
+	}
+
+	void OnPostRender ()
+	{
+		mMLight.enabled = false;
+	}
+
+}
