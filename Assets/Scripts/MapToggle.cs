b3bd415b76bcb7ce457879964248e13b977b9450
virtuahost <virtualghosts@gmail.com>
2014-11-16 14:52:09 -0500

    Working map commit

--- Assets/Scripts/MapToggle.cs
+++ Assets/Scripts/MapToggle.cs
@@ -2,32 +2,29 @@
 using System.Collections;
 
 public class MapToggle : MonoBehaviour {
-	public GameObject CameraRight;
-	public GameObject CameraLeft;
+	public GameObject MinimapQuad;
 	public GameObject OrgCameraRight;
 	public GameObject OrgCameraLeft;
 	public GameObject DirectionalLight;
 	private bool mapMode = false;
 	// Update is called once per frame
 	void Start(){		
-		CameraRight.camera.enabled = mapMode;
-		CameraLeft.camera.enabled = mapMode;
-		DirectionalLight.light.enabled = mapMode;
+		//MinimapCamera.camera.enabled = mapMode;
+		//DirectionalLight.light.enabled = mapMode;
 		
-		OrgCameraRight.camera.enabled = !mapMode;
-		OrgCameraLeft.camera.enabled = !mapMode;
+		//OrgCameraRight.camera.enabled = !mapMode;
+		//OrgCameraLeft.camera.enabled = !mapMode;
 		}
 
 	void Update () {
-		if (Input.GetKeyUp(KeyCode.M)){
-			mapMode = !mapMode;
-			
-			CameraRight.camera.enabled = mapMode;
-			CameraLeft.camera.enabled = mapMode;
-			DirectionalLight.light.enabled = mapMode;
-			
-			OrgCameraRight.camera.enabled = !mapMode;
-			OrgCameraLeft.camera.enabled = !mapMode;
-		}	
+		//if (Input.GetKeyUp(KeyCode.M)){
+		//	mapMode = !mapMode;
+		//	
+		//	MinimapCamera.camera.enabled = mapMode;
+		//	DirectionalLight.light.enabled = mapMode;
+		//	
+		//	OrgCameraRight.camera.enabled = !mapMode;
+		//	OrgCameraLeft.camera.enabled = !mapMode;
+		//}	
 	}
 }
--- Assets/Scripts/MinimapUpdater.cs
+++ Assets/Scripts/MinimapUpdater.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class MinimapUpdater : MonoBehaviour {
 	public Transform playerPosition;
-	public GameObject rightCamera;
+	public GameObject MinimapCamera;
 	// Update is called once per frame
 	void LateUpdate () {
 		//transform.position = new Vector3 (playerPosition.position.x, playerPosition.position.y, playerPosition.position.z);
-		//rightCamera.transform.position = new Vector3 (playerPosition.position.x, playerPosition.position.y, playerPosition.position.z);
+		MinimapCamera.transform.position = new Vector3 (playerPosition.position.x + 20.0f, playerPosition.position.y, playerPosition.position.z + 20.0f);
+
 	}
 }
