34f394edb09c09c3c8c1b5f898067d7d4ddde4eb
Arindam Bose <arindam.b@gatech.edu>
2014-11-19 10:32:10 -0500

    No more Co-routine bullshit!
    Why was I doing this again?

--- Assets/Scripts/ScaryCorridorBehaviour.cs
+++ Assets/Scripts/ScaryCorridorBehaviour.cs
@@ -27,6 +27,8 @@ public class ScaryCorridorBehaviour : MonoBehaviour {
 	private float createTImeStamp = 0f;
 	private float SecondsUntilCreate = 20f;
 	private GameObject player;
+	private Vector3 leftFinal;
+	private Vector3 rightFinal;
 	// Use this for initialization
 	void Start () {
 		leftWall=transform.FindChild("leftWall");
@@ -39,6 +41,8 @@ public class ScaryCorridorBehaviour : MonoBehaviour {
 		leftWallAudio=leftWall.FindChild("leftWallAudio").GetComponent<AudioSource>();
 		rightWallAudio=rightWall.FindChild("rightWallAudio").GetComponent<AudioSource>();
 		player = GameObject.FindWithTag ("playerForward").gameObject;	
+		leftFinal=leftWall.localPosition;
+		rightFinal=rightWall.localPosition;
 		leftWallParticles.Stop();
 		rightWallParticles.Stop();
 		
@@ -46,8 +50,17 @@ public class ScaryCorridorBehaviour : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	bool playerEntered=roomTrigger.GetComponent<PlayerCheckerScript>().isPlayerInside;
+	void FixedUpdate () {
+		bool playerEntered=roomTrigger.GetComponent<PlayerCheckerScript>().isPlayerInside;
+		playSound=leftWall.localPosition != leftFinal && rightWall.localPosition != rightFinal;
+		if(leftWall.localPosition != leftFinal && rightWall.localPosition != rightFinal)
+		{
+			
+			leftWall.localPosition = Vector3.MoveTowards(leftWall.localPosition, leftFinal, wallSpeed*Time.deltaTime);
+			rightWall.localPosition = Vector3.MoveTowards(rightWall.localPosition, rightFinal, wallSpeed*Time.deltaTime);
+			Debug.Log(leftWall.localPosition +" "+ rightWall.localPosition);
+			soundRoom.localScale = new Vector3((rightWall.localPosition.x - leftWall.localPosition.x)/initWallSeperation , 1, 1);
+		}
 		if(playerEntered)
 		{
 			if(playSound){
@@ -81,126 +94,39 @@ public class ScaryCorridorBehaviour : MonoBehaviour {
 					}
 				}
 			}
-			SetScare();
+		}else{
+				leftFinal=new Vector3(-initWallSeperation/2, 0, 0);
+				rightFinal=new Vector3(initWallSeperation/2, 0, 0);
+				if(leftWallParticles.isPlaying){
+					leftWallParticles.Stop();
+				}
+				if(rightWallParticles.isPlaying){
+					rightWallParticles.Stop();
+				}
+				if(leftWallAudio.isPlaying){
+					leftWallAudio.Stop();
+				}
+				if(rightWallAudio.isPlaying){
+					rightWallAudio.Stop();
+				}
+			
 		}
 	
 	}
 
-
-	public void SetScare()
-	{
-		int scareSelection=roomTrigger.GetComponent<PlayerCheckerScript>().scareMode;
-		if (scareSelection != -1) {
-			switch(scareSelection)
-			{
-			case 1:
-				//monster.audio.Play();
-				//skeleton.audio.Stop();
-				//pumpkin.audio.Stop();
-				break;
-			case 2:
-				//skeleton.audio.Play();
-				//monster.audio.Stop();
-				//pumpkin.audio.Stop();
-				break;
-			case 3:
-				if(createTImeStamp == 0f || (Time.time - createTImeStamp) > SecondsUntilCreate/currentSanity)
-				{					
-					pumpkin.SetActive(true);
-					Quaternion rotaPlay = player.transform.rotation;
-					Vector3 playerPos = new Vector3 (player.transform.position.x,1.7f,player.transform.position.z+10);
-					Instantiate(pumpkin,playerPos,rotaPlay);
-		            createTImeStamp = Time.time;					            
-					//pumpkin.audio.Play();					
-					pumpkin.SetActive(false);
-		        }
-				//monster.audio.Stop();
-				//skeleton.audio.Stop();
-				break;
-			default:
-				break;
-			}
-		}
-	}
-
-//	public void setSanity(float sanity){
-//		if(sanity!=currentSanity){
-//			if(!leftWallParticles.isPlaying){
-//				leftWallParticles.Play();
-//			}
-//			if(!rightWallParticles.isPlaying){
-//				rightWallParticles.Play();
-//			}
-//			if(!leftWallAudio.isPlaying){
-//				leftWallAudio.Play();
-//			}
-//			if(!rightWallAudio.isPlaying){
-//				rightWallAudio.Play();
-//			}
-//		}else{
-//			if(leftWallParticles.isPlaying){
-//				leftWallParticles.Stop();
-//			}
-//			if(rightWallParticles.isPlaying){
-//				rightWallParticles.Stop();
-//			}
-//			if(leftWallAudio.isPlaying){
-//				leftWallAudio.Stop();
-//			}
-//			if(rightWallAudio.isPlaying){
-//				rightWallAudio.Stop();
-//			}
-//		}
-//		currentSanity=sanity;
-//	}
-//	
-//	void setWallSeperation(float seperation){
-//		leftWall.localPosition = new Vector3(-seperation/2, 0, 0);
-//		rightWall.localPosition = new Vector3(seperation/2, 0, 0);
-//		soundRoom.localScale = new Vector3(seperation/initWallSeperation, 1, 1);
-//	}
 	public void setSanity(float sanity){
 		bool playerEntered=roomTrigger.GetComponent<PlayerCheckerScript>().isPlayerInside;
 		if(playerEntered){
 			float seperation=Mathf.Clamp(sanity*initWallSeperation/(maxSanity-minSanity),2,initWallSeperation);		
 			Vector3 leftInitial = leftWall.localPosition;
 			Vector3 rightInitial = rightWall.localPosition;
-			Vector3 leftFinal = new Vector3(-seperation/2, 0, 0);
-			//Debug.Log(leftFinal);
+			leftFinal = new Vector3(-seperation/2, 0, 0);
+			Debug.Log(leftFinal);
 			
-			Vector3 rightFinal = new Vector3(seperation/2, 0, 0);
-			//Debug.Log(rightFinal);
-			Vector3 soundInitial = soundRoom.localScale;
-			Vector3 soundFinal = new Vector3(seperation/initWallSeperation, 1, 1);
+			rightFinal = new Vector3(seperation/2, 0, 0);
+			Debug.Log(rightFinal);
 			
-		
-				if(isRunning){
-					stopRunning=true;
-	
-				}else{
-					StartCoroutine(moveWalls(leftInitial, leftFinal, rightInitial, rightFinal));
-				}
 		}
 		
-	}
-	
-	IEnumerator moveWalls(Vector3 leftInitial, Vector3 leftFinal, Vector3 rightInitial, Vector3 rightFinal){
-
-		isRunning=true;
-		playSound=true;
-		while(leftWall.localPosition != leftFinal && rightWall.localPosition !=rightFinal){
-			if(stopRunning){
-				stopRunning = false;
-				isRunning = false;
-				yield break;
-			}
-			leftWall.localPosition = Vector3.MoveTowards(leftWall.localPosition, leftFinal, wallSpeed*Time.deltaTime);
-			rightWall.localPosition = Vector3.MoveTowards(rightWall.localPosition, rightFinal, wallSpeed*Time.deltaTime);
-			Debug.Log(leftWall.localPosition +" "+ rightWall.localPosition);
-			soundRoom.localScale = new Vector3((rightWall.localPosition.x - leftWall.localPosition.x)/initWallSeperation , 1, 1);
-			yield return new WaitForSeconds(0.01f);	
-		}
-		isRunning=false;
-		playSound=false;
 	}
 }
